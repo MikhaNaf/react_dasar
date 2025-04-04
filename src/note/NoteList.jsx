05749a4845504cee63f3478edd3ebcb5dc833a8e
Mikha Naftali <mikhanaftali13@gmail.com>
2025-04-04 20:11:06 +0700

    Memo

--- src/note/NoteList.jsx
+++ src/note/NoteList.jsx
@@ -1,16 +1,32 @@
-import { useContext } from "react"
+import { useContext, useMemo, useRef, useState } from "react"
 import Note from "./Note"
 import { NotesContext } from "./NotesContext"
 
 export default function NoteList(){
     const notes = useContext(NotesContext)
+    const [search, setSearch] = useState("")
+    const searchInput = useRef(null)
+
+    const filteredNotes = useMemo(() => {
+        console.info('Filtering Notes')
+        return notes.filter(note => note.text.includes(search))
+    }, [notes, search])
+
+    function handleSearch(){
+        console.info("Search")
+        setSearch(searchInput.current.value)
+    }
     return(
-        <ul>
-            {notes.map(note => (
+        <div>
+            <input ref={searchInput} placeholder="Search"/>
+            <button onClick={handleSearch}>Search</button>
+            <ul>
+            {filteredNotes.map(note => (
                 <li key={note.id}>
                     <Note note={note}/>
                 </li>
             ))}
         </ul>
+        </div>
     )
 }
